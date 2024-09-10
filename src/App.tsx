8546ad26cf19a8769ac61bebd215aaf02acc28cf
likeacloud <aprimelonge@snu.ac.kr>
2024-09-10 21:54:55 +0900

    edit to fit the format

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import './App.css';
 
 function App() {
-    return (
-        <div className="profile-container">
-            <div className="profile-image">
-                <img src="src/assets/jerry.PNG" alt="Profile" />
-            </div>
-            <div className="profile-details">
-                <h2>박세준</h2>
-                <p>컴퓨터공학부</p>
-                <p>21학번</p>
-            </div>
-        </div>
-    );
+  return (
+    <div className="profile-container">
+      <div className="profile-image">
+        <img src="src/assets/jerry.PNG" alt="Profile" />
+      </div>
+      <div className="profile-details">
+        <h2>박세준</h2>
+        <p>컴퓨터공학부</p>
+        <p>21학번</p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
