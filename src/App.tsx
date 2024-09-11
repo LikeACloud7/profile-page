e31b5d6d1f721f5876ea155305bdfc3cf0a4f747
likeacloud <aprimelonge@snu.ac.kr>
2024-09-11 11:39:00 +0900

    profile update

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,19 @@ function App() {
   return (
     <div className="profile-container">
       <div className="profile-image">
-        <img src="src/assets/jerry.PNG" alt="Profile" />
+        <img 
+          src="src/assets/jerry.PNG" 
+          alt="Profile" 
+          className="profile-picture" 
+        />
       </div>
       <div className="profile-details">
-        <h2>박세준</h2>
-        <ul>
+        <h1 className="profile-name">박세준</h1>
+        <ul className="profile-info">
           <li>컴퓨터공학부 21학번</li>
-          <li>INFP</li>
-          <li>뭘 하고 싶은지 방황중</li>
-          <li>다들 건강하시고 행복하세요~</li>
+          <li>현재는 저널리즘, 정치 관심 많음</li>
+          <li>요즘 뭘 하고 싶은지 방황중</li>
+          <li>다들 건강하고 행복하시길~</li>
         </ul>
       </div>
     </div>
